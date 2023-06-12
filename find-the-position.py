f359e79e14a2f7bf3158fb03d4f140008c1fa814
Jeremy <jeremy.quihano@gmail.com>
2023-06-12 08:55:46 -0700

    add my solution

--- find-the-position.py
+++ find-the-position.py
@@ -3,3 +3,9 @@
 # Input :: "a"
 
 # Ouput :: "Position of alphabet: 1"
+
+def position(alphabet):
+    alphabet_char = "abcdefghijklmnopqrstuvwxyz"
+    for i in range(len(alphabet_char)):
+        if alphabet_char[i] == alphabet:
+            return 'Position of alphabet: %s' %(str(i+1))
